99f9f4502a138f262d5510132bbfd8d5bc841ce3
klj lk <klj lk>
2017-06-15 23:26:09 +0530

    text change common for about us and discliajmer

--- DemoApplication/app/src/main/java/com/Fragment/AboutUsFragment.java
+++ DemoApplication/app/src/main/java/com/Fragment/AboutUsFragment.java
@@ -3,16 +3,22 @@ package com.Fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import com.greencardgo.R;
 
 
 public class AboutUsFragment extends Fragment {
+
+
+    private String text;
+    private WebView webview;
+
 
     public static AboutUsFragment newInstance(Context context) {
 
@@ -23,12 +29,20 @@ public class AboutUsFragment extends Fragment {
         return fragment;
     }
 
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about_us, null, false);
+        webview = (WebView) view.findViewById(R.id.webview);
+        text=getActivity().getResources().getString(R.string.about_us_text);
+        setData();
+
         return view;
     }
 
 
+    private void setData() {
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadDataWithBaseURL("", text, "text/html", "UTF-8", "");
+    }
 }
+
